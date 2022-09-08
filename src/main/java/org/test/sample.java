f71d0434799e81eee43f4b917ff6123fa9702c9c
balajisrinivasan12 <balajisrinivasan1212@gmail.com>
2022-09-08 13:01:27 +0530

    Resolved Conflicts

---
+++ src/main/java/org/test/sample.java
@@ -0,0 +1,34 @@
+package org.test;
+
+public class sample {
+public static void main(String[] args) {
+	System.out.println("Test 1");
+	
+	<<<<<HEAD
+	System.out.println("Test 2");
+	System.out.println("Test 3");
+	""""""""""
+	
+	System.out.println("Test 4");
+	System.out.println("Test 5");
+	
+	
+	
+	
+	
+	
+	
+}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
